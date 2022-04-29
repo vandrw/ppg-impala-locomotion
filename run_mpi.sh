60724831803388ae5e6601086a165fd74f552693
vandrw <v.andrei1455@gmail.com>
2022-04-29 19:53:11 +0200

    jobscript updates for mpi

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=myleg_ppg_mpi
 #SBATCH --time=24:00:00
-#SBATCH --ntasks=64
-#SBATCH --mem=196GB
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=24
+#SBATCH --mem=16GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=
 #SBATCH --partition=regular
@@ -14,6 +15,6 @@ source /data/$USER/.envs/osim/bin/activate
 export LD_LIBRARY_PATH=/data/$USER/.libs/opensim_dependencies/ipopt/lib:/data/$USER/.libs/opensim_dependencies/adol-c/lib64:$LD_LIBRARY_PATH
 
 # Change the number after -n to increase or lower the number of workers used.
-mpirun -n 58 python -m src.train_mpi -c configs/healthy.yml --run-name healthy_mpi
+mpirun -n 64 python -m src.train_mpi -c configs/healthy.yml --run-name healthy_mpi
 
 deactivate
