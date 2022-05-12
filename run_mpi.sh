6c34723d60ff5970134f71e4000d4c65ea581519
C.A.Voinea@student.rug.nl <s3754243@pg-interactive.hpc.rug.nl>
2022-05-12 20:39:59 +0200

    Add epoch as step for wandb

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=myleg_ppg_mpi
 #SBATCH --time=24:00:00
-#SBATCH --output=healthy_mpi_%j.out
+#SBATCH --output=logs/healthy_mpi_%j.out
 #SBATCH --nodes=3
 #SBATCH --ntasks=64
-#SBATCH --mem=16GB
+#SBATCH --mem=32GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=
 #SBATCH --partition=regular
@@ -15,7 +15,7 @@ module load PyTorch/1.10.0-fosscuda-2020b
 source /data/$USER/.envs/osim/bin/activate
 export LD_LIBRARY_PATH=/data/$USER/.libs/opensim_dependencies/ipopt/lib:/data/$USER/.libs/opensim_dependencies/adol-c/lib64:$LD_LIBRARY_PATH
 
-# Add the option -n to increase or lower the number of workers used.
+# Add the option -n after 'mpirun' to increase or lower the number of workers used.
 mpirun --mca opal_warn_on_missing_libcuda 0 python -m src.train_mpi -c configs/healthy.yml --run-name healthy_mpi
 
 deactivate
