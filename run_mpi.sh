9b0adb7bd58068ed723ed4de6e78c64af1f13c04
C.A.Voinea@student.rug.nl <s3754243@pg-interactive.hpc.rug.nl>
2022-05-21 22:09:57 +0200

    Jobscript update

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=myleg_ppg_mpi
-#SBATCH --time=24:00:00
+#SBATCH --time=7-00:00
 #SBATCH --output=output/healthy_mpi_%j.out
-#SBATCH --nodes=3
-#SBATCH --ntasks=64
+#SBATCH --nodes=2
+#SBATCH --ntasks=48
 #SBATCH --mem=32GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=
@@ -15,7 +15,6 @@ module load PyTorch/1.10.0-fosscuda-2020b
 source /data/$USER/.envs/osim/bin/activate
 export LD_LIBRARY_PATH=/data/$USER/.libs/opensim_dependencies/ipopt/lib:/data/$USER/.libs/opensim_dependencies/adol-c/lib64:$LD_LIBRARY_PATH
 
-# Add the option -n after 'mpirun' to increase or lower the number of workers used.
-mpirun --mca opal_warn_on_missing_libcuda 0 python -m src.train_mpi -c configs/healthy.yml --run_name healthy_mpi
+mpirun --mca opal_warn_on_missing_libcuda 0 python -m src.train_mpi -c configs/healthy.yml
 
 deactivate
