5c4e5839a4c520286cae724524073434b36fc314
Andrei Voinea <v.andrei1455@gmail.com>
2022-06-16 12:45:42 +0200

    Debug.

--- src/ppg/learner.py
+++ src/ppg/learner.py
@@ -118,10 +118,17 @@ class Learner:
         self.policy_optimizer.zero_grad()
         self.value_optimizer.zero_grad()
 
+        if loss.isnan().any():
+            print("loss is nan!")
+
         loss.backward()
 
         self.policy_optimizer.step()
         self.value_optimizer.step()
+
+        for d in self.policy.state_dict().values():
+            if d.isnan().any():
+                print(d)
 
     def training_aux(self, states):
         Returns = self.value(states).detach()
